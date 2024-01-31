89e5c66b6e937ea45e8fdb3c9868b5e6aa2c4f0e
Ruslan Kidun <ruslan.kidun@gmail.com>
2024-01-31 22:54:23 +0300

    cleaner code, using window frames

--- 028.find_the_index_of_the_first_occurrence_in_a_string/main.go
+++ 028.find_the_index_of_the_first_occurrence_in_a_string/main.go
@@ -8,13 +8,8 @@ func strStr(haystack string, needle string) int {
 
 	// position cannot be more than haystack_end-len(needle)
 	for i := 0; i <= len(haystack)-len(needle); i++ {
-		//compare substring from position "i"
-		for j := 0; j < len(needle); j++ {
-			if needle[j] != haystack[i+j] {
-				break
-			} else if j >= len(needle)-1 {
-				return i
-			}
+		if haystack[i:i+len(needle)] == needle {
+			return i
 		}
 	}
 	return -1
