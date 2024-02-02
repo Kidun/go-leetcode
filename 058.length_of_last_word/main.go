0670581e809c12dd1213e567a86d2e822d94cf09
Ruslan Kidun <ruslan.kidun@gmail.com>
2024-02-02 11:08:35 +0300

    058.length_of_last_word solution

--- 058.length_of_last_word/main.go
+++ 058.length_of_last_word/main.go
@@ -1,7 +1,21 @@
 package main
 
 func lengthOfLastWord(s string) int {
-
+	end_pos := -1
+	// iterating the string from the end until we find the end of word
+	// then proceed until we find the beginning
+	for i := len(s) - 1; i >= 0; i-- {
+		if end_pos < 0 {
+			if s[i] != ' ' {
+				end_pos = i
+			}
+		} else {
+			if s[i] == ' ' {
+				return end_pos - i
+			}
+		}
+	}
+	return end_pos + 1
 }
 
 func main() {
--- 058.length_of_last_word/main_test.go
+++ 058.length_of_last_word/main_test.go
@@ -14,15 +14,20 @@ func Test_lengthOfLastWord(t *testing.T) {
 	}
 
 	s = "   fly me   to   the moon  "
-	res := lengthOfLastWord(s)
+	res = lengthOfLastWord(s)
 	if res != 4 {
 		t.Error("unexpected value for (" + s + "), result |" + strconv.Itoa(res) + "|")
 	}
 
 	s = "luffy is still joyboy"
-	res := lengthOfLastWord(s)
+	res = lengthOfLastWord(s)
 	if res != 6 {
 		t.Error("unexpected value for (" + s + "), result |" + strconv.Itoa(res) + "|")
 	}
 
+	s = "luffyisstilljoyboy"
+	res = lengthOfLastWord(s)
+	if res != 18 {
+		t.Error("unexpected value for (" + s + "), result |" + strconv.Itoa(res) + "|")
+	}
 }
