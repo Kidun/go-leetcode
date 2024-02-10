c83ec5661b197f430b69d4f46a4e485cbff7215b
Ruslan Kidun <ruslan.kidun@gmail.com>
2024-02-10 16:40:42 +0300

    121.best_time_to_buy_and_sell_stock brute-force solution O(n^2)/2

--- 121.best_time_to_buy_and_sell_stock/main.go
+++ 121.best_time_to_buy_and_sell_stock/main.go
@@ -1,8 +1,18 @@
 package main
 
 func maxProfit(prices []int) int {
-
-	return 0
+	maxprofit := 0
+	for i := 0; i < len(prices)-1; i++ {
+		for j := i + 1; j < len(prices); j++ {
+			if prices[j] > prices[i] {
+				profit := prices[j] - prices[i]
+				if profit > maxprofit {
+					maxprofit = profit
+				}
+			}
+		}
+	}
+	return maxprofit
 }
 
 func main() {
