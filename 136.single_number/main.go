f134f51c0314f0369559adab2ef51fe5bfc6c60d
Ruslan Kidun <ruslan.kidun@gmail.com>
2024-02-12 12:04:19 +0300

    136.single_number hashmap flexible to changes solution

--- 136.single_number/main.go
+++ 136.single_number/main.go
@@ -1,6 +1,20 @@
 package main
 
 func singleNumber(nums []int) int {
+	numsMap := map[int]int{}
+	for _, val := range nums {
+		_, ok := numsMap[val]
+		if ok {
+			delete(numsMap, val)
+		} else {
+			numsMap[val] = 1
+		}
+	}
+	for val, count := range numsMap {
+		if count == 1 {
+			return val
+		}
+	}
 	return 0
 }
 
