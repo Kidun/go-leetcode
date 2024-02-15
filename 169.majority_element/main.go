396d8fd3b8bf84ef87d8c31b2af1ce1710cb58c1
Ruslan Kidun <ruslan.kidun@gmail.com>
2024-02-15 22:32:40 +0300

    169.majority_element voting algo solution

--- 169.majority_element/main.go
+++ 169.majority_element/main.go
@@ -1,15 +1,33 @@
 package main
 
 func majorityElement(nums []int) int {
-	numsMap := map[int]int{}
-	var res, maxCounter int
-	for _, num := range nums {
-		numsMap[num]++
-		if numsMap[num] > maxCounter {
-			maxCounter = numsMap[num]
-			res = num
+	// hashmap solution
+	// numsMap := map[int]int{}
+	// half := len(nums) / 2
+	res, maxCounter := nums[0], 1
+	// numsMap[nums[0]] = 1
+	for i := 1; i < len(nums); i++ {
+		// numsMap[nums[i]]++
+		// if numsMap[nums[i]] > maxCounter {
+		// 	maxCounter, res = numsMap[nums[i]], nums[i]
+		// 	if maxCounter > half {
+		// 		return res
+		// 	}
+		// }
+
+		// solution that exploits >⌊n/2⌋ condition
+		// Boyer-Moore Majority Vote Algorithm
+		if maxCounter == 0 {
+			res, maxCounter = nums[i], 1
+		} else {
+			if nums[i] == res {
+				maxCounter++
+			} else {
+				maxCounter--
+			}
 		}
 	}
+
 	return res
 }
 
