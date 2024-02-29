f1ee8ee59e11fc3de07b1fda0c7ad43ad781699d
Ruslan Kidun <ruslan.kidun@gmail.com>
2024-02-29 23:04:38 +0300

    278.first_bad_version bin-search solution

--- 278.first_bad_version/main.go
+++ 278.first_bad_version/main.go
@@ -1,10 +1,12 @@
 package firstBadVersion
 
+// mock function
 func isBadVersion(version int) bool {
 	return version > 123
 }
 
 func firstBadVersion(n int) int {
+	// O(log(n)) binary search
 	left := 1
 	for left < n {
 		mid := (left + n) / 2
